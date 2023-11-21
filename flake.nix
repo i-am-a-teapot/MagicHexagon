fc4ecfda740024abcff7cb9fa69ec0ee76747d66
Swarsel <leon.schwarzaeugl@gmail.com>
2023-11-21 15:31:21 +0100

    add perf to devShell

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
-  description = "Basic C++ project";
+  description = "Magic Hexagon project";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.05";
-
     utils.url = "github:numtide/flake-utils";
-    utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+  outputs = inputs@{ self, nixpkgs, ... }: inputs.utils.lib.eachSystem [
     "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
   ] (system: let pkgs = import nixpkgs {
                    inherit system;
@@ -20,6 +18,7 @@
                  packages = with pkgs; [
                    # Development Tools
                    llvmPackages_16.clang
+                   linuxPackages_latest.perf
                    cmake
                    cmakeCurses
                  ];
